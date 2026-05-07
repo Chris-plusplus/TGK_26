7e8e1d8a54b8c6bbbe70b143599f3e7c674147de
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-05-07 23:46:11 +0200

    removed Acceleration from collided cans

--- src/AccelerationSystem.cpp
+++ src/AccelerationSystem.cpp
@@ -2,23 +2,19 @@
 #include <archimedes/Scene.h>
 #include <box2d/box2d.h>
 #include <SlingshotSystem.h>
+#include <CollisionSystem.h>
 
 using namespace arch;
 
 void AccelerationSystem::update() {
-	if (input::Mouse::left.pressed()) {
-		auto&& domain = scene::SceneManager::get()->currentScene()->domain();
-		for (auto&& [can, acc, t, body] : domain.view<Acceleration, scene::components::TransformComponent, b2BodyId>(exclude<Can>).all()) {
-			auto velBefore = b2Length(b2Body_GetLinearVelocity(body));
-
-			b2Body_SetLinearVelocity(body, acc.value * b2Normalize(b2Body_GetLinearVelocity(body)));
-
-			auto velAfter = b2Length(acc.value * b2Normalize(b2Body_GetLinearVelocity(body)));
-
-			Logger::debug("\nacc: {:.2f} -> {:.2f}", velBefore, velAfter);
-
+	auto&& domain = scene::SceneManager::get()->currentScene()->domain();
+	for (auto&& [can, acc, t, body] : domain.view<Acceleration, scene::components::TransformComponent, b2BodyId>(exclude<Can>).all()) {
+		if (domain.hasComponent<Collided>(can)) {
 			domain.removeComponent<Acceleration>(can);
-
+			break;
+		} else if (input::Mouse::left.pressed()) {
+			b2Body_SetLinearVelocity(body, acc.value * b2Normalize(b2Body_GetLinearVelocity(body)));
+			domain.removeComponent<Acceleration>(can);
 			break;
 		}
 	}
