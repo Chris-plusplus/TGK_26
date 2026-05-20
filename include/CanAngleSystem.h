fef695cb1296576385086233defe4acc55a45b12
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2026-05-20 10:40:40 +0200

    can now angle on slingshot and flight

---
+++ include/CanAngleSystem.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <archimedes/Scene.h>
+
+using namespace arch;
+
+struct CanAngleSystem {
+	static void update();
+};
---
+++ src/CanAngleSystem.cpp
@@ -0,0 +1,31 @@
+#include <CanAngleSystem.h>
+#include <SlingshotSystem.h>
+#include <Defaults.h>
+#include <CollisionSystem.h>
+
+void CanAngleSystem::update() {
+	auto&& scene = *scene::SceneManager::get()->currentScene();
+	auto&& domain = scene.domain();
+	auto&& [entity, slingshot] = domain.view<Slingshot>().all().front();
+
+	if (slingshot.state == slingshot.dragged) {
+		if (input::Mouse::left.down()) {
+			auto&& [canEntity, can, canos, t, body] = domain.view<Can, CanOnSlingshot, scene::components::TransformComponent, b2BodyId>().all().front();
+			auto dpos = slingshot.centerPos - float3(input::Mouse::pos(), 0);
+
+			float angle = std::atan2(dpos.y, dpos.x);
+
+			t.rotation = glm::angleAxis(angle, zAxis());
+			b2Body_SetTransform(body, b2Body_GetPosition(body), b2MakeRot(angle));
+		}
+	}
+
+	for (auto&& [t, body] : domain.view<scene::components::TransformComponent, b2BodyId, Launched>(exclude<Collided>).components()) {
+		auto velNorm = b2Normalize(b2Body_GetLinearVelocity(body));
+
+		auto angle = std::atan2(velNorm.y, velNorm.x);
+
+		t.rotation = glm::angleAxis(angle, zAxis());
+		b2Body_SetTransform(body, b2Body_GetPosition(body), b2MakeRot(angle));
+	}
+}
